c2e2bbfcddd038f8eb9f3baf68c4bac5c791f3b7
WangZhenghai <fisschl@163.com>
2025-07-21 12:16:46 +0800

    refactor(uuid): 更新UUID解析函数名称以提高代码一致性
    将UUID解析函数名称从uuidParse更改为uuid2Bytes，确保代码的可读性和一致性。同时更新相关调用处以反映此更改。

--- utils/uuid.ts
+++ utils/uuid.ts
@@ -1,10 +1,10 @@
 import { base58 } from "@scure/base";
-import { parse as uuidParse, v7 } from "uuid";
+import { parse as uuid2Bytes, v7 } from "uuid";
 
 export const uuid = () => v7();
 
 export const newToken = () => {
-  const uuidBytes = uuidParse(uuid());
+  const uuidBytes = uuid2Bytes(uuid());
   const randomBytes = new Uint8Array(13);
   crypto.getRandomValues(randomBytes);
   const bytes = new Uint8Array([...uuidBytes, ...randomBytes]);
