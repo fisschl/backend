6c8ada6963f47edbe74368f41b85e20d93a8cd70
WangZhenghai <fisschl@163.com>
2025-07-26 19:12:07 +0800

    在index.ts中移除静态文件处理逻辑中的Content-Length头部设置，以简化响应头部信息。

--- api/static/index.ts
+++ api/static/index.ts
@@ -6,11 +6,10 @@ export const staticRouter = new H3().get("/**", async (event) => {
   const { pathname } = getRequestURL(event);
   const s3Path = pathname.slice(pathname.indexOf("/static/"));
   const file = s3.file(s3Path);
-  const { type, size } = await file.stat();
+  const { type } = await file.stat();
   const stream = file.stream();
   const headers = new Headers();
   headers.set("Cache-Control", `public, max-age=${daysToSeconds(30)}`);
   headers.set("Content-Type", type);
-  headers.set("Content-Length", size.toString());
   return new Response(stream, { headers });
 });
